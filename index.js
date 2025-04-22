ea79f30ecd158fcd83b8e3c45160abbc2110e0d5
Boo-coder <Bouna112@yahoo.com>
2025-04-21 23:00:51 -0400

    sdDF

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email){
+    const userData = {
+        name: name,
+        email: email
+    };
+    return fetch('http://localhost:3000/users', {  
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        body: JSON.stringify(userData),
+         
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("Success:", data)
+        const contentElement = document.getElementById("content")
+        const newUserId = document.createElement("p")
+        newUserId.textContent = `rando: ${data.id}`
+        contentElement.appendChild(newUserId)
+    })
+        
+    .catch(error => {
+        console.error('Error:', error)
+        const contentElement = document.getElementById("content")
+        const errorMessage = document.createElement("p")
+        errorMessage.textContent = `Unauthorized Access: ${error.message}`
+        contentElement.appendChild(errorMessage)
+    });
+}
+
+submitData('Steve', 'steve@steve.com');
